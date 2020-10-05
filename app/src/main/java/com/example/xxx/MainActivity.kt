896550c6db54e70275ce151929aa5e88d741e3d3
thomas.funke@s-markt-mehrwert.de <thomas.funke@s-markt-mehrwert.de>
2020-10-05 13:21:52 +0200

    datetime

--- app/src/main/java/com/example/xxx/MainActivity.kt
+++ app/src/main/java/com/example/xxx/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.xxx
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,13 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val today = Calendar.getInstance()
+
+        //ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+
+        val sendDateUAT = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(today.time) // 2020-09-11T13:11:50.375+00:00
+        Log.i("xxxx", sendDateUAT + "; " + today.timeZone)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
